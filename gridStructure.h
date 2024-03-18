fe507030aa4f75e22c2f3040a7f34fc5c27a6826
Ahnaaf Khan <ahnaafk@gmail.com>
2024-03-18 13:23:10 -0400

    Create movement function

--- gridStructure.h
+++ gridStructure.h
@@ -3,10 +3,11 @@
 typedef struct cell
 {
     int coordinates[2]; 
-    int cellData[6]; //one-hot encoded array where 0 is false, and 1 is true. 
+    int cellData[4]; //one-hot encoded array where 0 is false, and 1 is true. 
     pCell *passenger;
     pCell *destination;
 } Cell, *pCell;
 
 Cell grid[10][10];
 
+int move(int direction[2], int coordinates[2]);
