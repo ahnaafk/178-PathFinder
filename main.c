45434f292fe5649de3c513af66bdfaac061f5301
Ahnaaf Khan <ahnaafk@gmail.com>
2024-03-27 23:57:09 -0400

    Add printgrid function to main. stably prints now

--- main.c
+++ main.c
@@ -4,22 +4,31 @@
 #include "pathFinder.h"
 #include <time.h>
 
-void printGrid(Cell grid[GRID_SIZE][GRID_SIZE]) {
-     for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if (grid[i][j].cellData[OPEN] == TRUE) printf("0");
-            if (grid[i][j].cellData[CONSTRUCTION] == TRUE) printf("1");
-            if (grid[i][j].cellData[PASSENGER] == TRUE) printf("p");
-            if (grid[i][j].cellData[DESTINATION] == TRUE) printf("d");
+void printGrid(Cell grid[GRID_SIZE][GRID_SIZE])
+{
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            if (grid[i][j].cellData[OPEN] == TRUE)
+                printf("0");
+            if (grid[i][j].cellData[CONSTRUCTION] == TRUE)
+                printf("1");
+            if (grid[i][j].cellData[PASSENGER] == TRUE)
+                printf("p");
+            if (grid[i][j].cellData[DESTINATION] == TRUE)
+                printf("d");
         }
         printf("\n");
     }
 }
 
-int main () {
+int main()
+{
     srand(time(NULL));
     Cell grid[GRID_SIZE][GRID_SIZE];
     createGrid(grid);
-   
+    printGrid(grid);
+
     return 0;
 }
